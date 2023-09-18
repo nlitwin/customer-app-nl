197a0e6605a6b65a090a6612cded22ebc9c10f5a
Nick Litwin <nlitwin@users.noreply.github.com>
2023-09-18 16:04:20 +0100

    Add example hitting API directly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-// import { useState } from "react"
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [result, setResult] = useState(null)
+  const apiKey = "80569edbdcd93e9f8e3660c17de9f3e8"
+  const clientId = "utahcounty.gov"
+  const handleChange = async (e) => {
+    const file = e.target.files[0]
+    console.log(e.target.files[0])
+    const data = new FormData()
+    data.append("file", file)
+    try {
+      setResult(null)
+      const response = await fetch("https://titanstage.herokuapp.com/v2/seals/verify", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "X-API-Key": apiKey,
+          "X-Client-Id": clientId,
+        },
+        body: data,
+      })
+
+      const jsonResponse = await response.json()
+      console.log(jsonResponse)
+      setResult(jsonResponse)
+    } catch (e) {
+      setResult(e)
+      console.error(e)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <div id="titan-seal-verify" data-api-key="123" data-client-id="titanseal.com" data-env="staging"></div>
+        <p>Using the Titan Seal verification component</p>
+        <div id="titan-seal-verify" data-api-key={apiKey} data-client-id={clientId} data-env="staging"></div>
+        <p>Custom UI code hitting the Titan Seal API directly</p>
+        <input type="file" onChange={handleChange} />
+        <p>Result of upload:</p>
+        <pre>{JSON.stringify(result, null, 2)}</pre>
       </header>
     </div>
   )
